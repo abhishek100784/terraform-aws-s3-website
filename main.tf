041fbe46e4741a5d529aac2819966534a04b9009
abhishek100784 <abhishek.aggarwal@enghouse.com>
2023-10-17 19:05:04 +0200

    Updated Modules files

--- main.tf
+++ main.tf
@@ -1,30 +1,67 @@
 # Create S3 Bucket Resource
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
-  acl    = "public-read"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": [
-              "s3:GetObject"
-          ],
-          "Resource": [
-              "arn:aws:s3:::${var.bucket_name}/*"
-          ]
-      }
-  ]
-}  
-EOF
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+
   tags          = var.tags
   force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.s3_bucket_public_access_block,
+  ]
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = data.aws_iam_policy_document.s3_bucket_iam_policy_document.json
+}
+
+data "aws_iam_policy_document" "s3_bucket_iam_policy_document" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_bucket_website_configuration" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
 }
 
